212ced5314593adcf71292556003416bae1dd6cb
perfogic <longdieu12x@gmail.com>
2024-10-12 23:10:43 +0700

    remove full bitmap test and comment error code on scripts

--- scripts/incrementCounter.ts
+++ scripts/incrementCounter.ts
@@ -1,5 +1,5 @@
 import { Address, toNano } from '@ton/core';
-import { Counter } from '../wrappers/Counter';
+// import { Counter } from '../wrappers/Counter';
 import { NetworkProvider, sleep } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider, args: string[]) {
@@ -12,25 +12,25 @@ export async function run(provider: NetworkProvider, args: string[]) {
     return;
   }
 
-  const counter = provider.open(Counter.createFromAddress(address));
+  // const counter = provider.open(Counter.createFromAddress(address));
 
-  const counterBefore = await counter.getCounter();
+  // const counterBefore = await counter.getCounter();
 
-  await counter.sendIncrease(provider.sender(), {
-    increaseBy: 1,
-    value: toNano('0.05'),
-  });
+  // await counter.sendIncrease(provider.sender(), {
+  //   increaseBy: 1,
+  //   value: toNano('0.05'),
+  // });
 
-  ui.write('Waiting for counter to increase...');
+  // ui.write('Waiting for counter to increase...');
 
-  let counterAfter = await counter.getCounter();
-  let attempt = 1;
-  while (counterAfter === counterBefore) {
-    ui.setActionPrompt(`Attempt ${attempt}`);
-    await sleep(2000);
-    counterAfter = await counter.getCounter();
-    attempt++;
-  }
+  // let counterAfter = await counter.getCounter();
+  // let attempt = 1;
+  // while (counterAfter === counterBefore) {
+  //   ui.setActionPrompt(`Attempt ${attempt}`);
+  //   await sleep(2000);
+  //   counterAfter = await counter.getCounter();
+  //   attempt++;
+  // }
 
   ui.clearActionPrompt();
   ui.write('Counter increased successfully!');
