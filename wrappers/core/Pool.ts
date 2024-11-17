a083b48b5e6fbe5542865c0b2c8ac7c68a641863
meomeocoj <minh.nt@orai.io>
2024-11-17 09:01:43 +0700

    feat: draft add-lp

--- wrappers/core/Pool.ts
+++ wrappers/core/Pool.ts
@@ -6,18 +6,18 @@ import {
   contractAddress,
   ContractProvider,
   Dictionary,
-  DictionaryKey,
-  DictionaryValue,
   Sender,
   SendMode,
 } from '@ton/core';
 import { crc32, ValueOps } from '..';
+import { InMsgBody, storeInMsgBody } from '../../tlb/pool/messages';
 
 namespace PoolWrapper {
   export const Opcodes = {
     Mint: crc32('op::mint'),
     Swap: crc32('op::swap'),
     Burn: crc32('op::burn'),
+    CallBackLiquidity: crc32('op::cb_add_liquidity'),
   };
 
   export interface InstantiateMsg {
@@ -75,7 +75,7 @@ namespace PoolWrapper {
         )
         .storeRef(
           beginCell()
-            .storeUint(0n, 64)
+            .storeUint(0n, 256)
             .storeRef(
               beginCell()
                 .storeDict(Dictionary.empty(Dictionary.Keys.Int(16), Dictionary.Values.Cell()))
@@ -99,6 +99,16 @@ namespace PoolWrapper {
       });
     }
 
+    async sendMint(provider: ContractProvider, via: Sender, value: bigint, inMsgBody: InMsgBody) {
+      const body = beginCell();
+      storeInMsgBody(inMsgBody)(body);
+      await provider.internal(via, {
+        value,
+        sendMode: SendMode.PAY_GAS_SEPARATELY,
+        body: body.endCell(),
+      });
+    }
+
     async getCollectedFees(provider: ContractProvider): Promise<bigint[]> {
       const result = await provider.get('get_collected_fees', []);
       const tuple = result.stack;
@@ -110,6 +120,30 @@ namespace PoolWrapper {
         }
       }
       return data;
+    }
+
+    async getLpAccountAddress(
+      provider: ContractProvider,
+      user: Address,
+      tick_lower: bigint,
+      tick_upper: bigint,
+    ): Promise<Address> {
+      const result = await provider.get('get_lp_account_address', [
+        {
+          type: 'slice',
+          cell: beginCell().storeAddress(user).endCell(),
+        },
+        {
+          type: 'int',
+          value: tick_lower,
+        },
+        {
+          type: 'int',
+          value: tick_upper,
+        },
+      ]);
+
+      return result.stack.readAddress();
     }
   }
 }
